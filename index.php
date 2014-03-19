0f76e8ec63a6136e2cd6b3627f52a28964312f4c
acoprocter <acoprocter@gmail.com>
2014-03-20 12:06:23 +1300

    initial commit

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+	
+	
+	<!-- Website Title & Description for Search Engine purposes -->
+	
+   
+    <title>WriteMaps | Create Sitemaps Online</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		
+    <meta name="description" content="WriteMaps | Make, create, and build sitemaps | Plan websites | Gather content">
+    <meta name="author" content="writemaps.com">
+
+    <!-- Bootstrap CSS -->
+
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="includes/font-awesome-4.0.3/css/font-awesome.min.css" rel="stylesheet"> 
+	<link href="includes/css/bootstrap-glyphicons.css" rel="stylesheet">
+	
+	<!-- Custom CSS -->
+	<link href="includes/css/styles.css" rel="stylesheet">
+
+
+    <!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+
+	<!-- Include Modernizr in the head, before any other Javascript -->
+	<script src="includes/js/modernizr-2.6.2.min.js"></script>
+
+	</head>
+
+
+  <body data-spy="scroll" data-target=".top-spy">
+  	
+	
+ 
+<!-- HEADER --> 
+
+
+  <nav class="navbar navbar-default navbar-inverse" id="top-nav" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="images/wmlogo.png" alt="Your Logo" /></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-responsive-collapse" id="bs-example-navbar-collapse-1">
+      
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a data-toggle="modal" class="btn" href="#myModal" role="button">Log in</a></li>
+	</ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+</nav>
+
+
+<div class="modal fade" id="myModal">
+							<div class="container">
+    <div class="row vertical-offset-100">
+    	<div class="col-md-4 col-md-offset-4">
+    		<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<button class="close" data-dismiss="modal">&times;</button>
+			    	<h3 class="panel-title">Log In</h3>
+			 	</div>
+			  	<div class="panel-body">
+			    	<form accept-charset="UTF-8" role="form">
+                    <fieldset>
+			    	  	<div class="form-group">
+			    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
+			    		</div>
+			    		<div class="checkbox">
+			    	    	<label>
+			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
+			    	    	</label>
+			    	    </div>
+			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+			    		<button type="button" class="btn btn-link">Forgot your password?</button>
+			    	</fieldset>
+			      	</form>
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
+		</div> <!-- end myModal -->					
+							
+							
+			
+    
+<!-- end HEADER -->
+
+
+<div class="row" id="row1" >
+	<div class="container">
+		<div class="col-sm-6">
+	
+		<h1>Plan websites fast</h1>
+		<h2>WriteMaps helps you to create <span class="amp">&amp;</span> share your sitemaps online</h2>
+		
+		<a class="btn btn-large btn-block btn-warning" id="btn-get-started" href="#">Get Started</a>
+	
+		</div> <!-- end col-sm-6 -->
+	
+		<div class="col-sm-6">
+	
+		<iframe src="http://player.vimeo.com/video/67412259?title=0&amp;byline=0&amp;portrait=0" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen ></iframe>		
+		</div> <!-- end col-sm-6 -->
+	</div> <!-- end container -->
+	
+</div> <!-- end row1 -->
+
+
+
+
+<div class="row" id="row2">
+	<div class="container">
+	<div class="carousel slide" id="myCarousel" data-ride="carousel">
+			
+			<!-- Indicators -->
+			<!-- <ol class="carousel-indicators">
+				<li class="active" data-slide-to="0" data-target="#myCarousel"></li>
+				<li data-slide-to="1" data-target="#myCarousel"></li>
+				<li data-slide-to="2" data-target="#myCarousel"></li>
+			</ol>
+		-->
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active" id="slide1">
+					
+						<img src="images/carousel/writemaps-slide1.png" class="img-responsive" >
+				</div> <!-- end item -->
+				
+				<div class="item" id="slide2">
+					<img src="images/carousel/writemaps-slide2.png" class="img-responsive" >
+				</div> <!-- end item -->
+				
+				<div class="item" id="slide3">
+					<img src="images/carousel/writemaps-slide3.png" class="img-responsive" >
+				</div> <!-- end item -->
+				
+				<div class="item" id="slide3">
+					<img src="images/carousel/writemaps-slide4.png" class="img-responsive" >
+				</div> <!-- end item -->
+				
+			</div> <!-- end carousel-inner -->
+			
+			<!-- Controls -->
+			<a class="left carousel-control" data-slide="prev" href="#myCarousel"><span class="icon-prev"></span></a>
+			<a class="right carousel-control" data-slide="next" href="#myCarousel"><span class="icon-next"></span></a>
+		
+	</div> <!-- end container -->
+		</div> <!-- end of carousel -->
+
+<!-- end row2 -->
+
+<div class="row" id="features">
+			<div class="container">
+					<div class="col-sm-4 feature">
+					<div class="panel">
+						<div class="panel-heading">
+							<h3 class="panel-title"><span class="glyphicon glyphicon-dashboard"></span> Reduce Project Time</h3>
+						</div> <!-- end panel-heading -->
+						
+						
+						<p>Save time by planning a website with a process. Create sitemaps and gather content with WriteMaps to work through the same steps every time, and achieve the same great results.</p>
+						
+						
+					</div> <!-- end panel -->
+					
+				</div> <!-- end col-sm-4 -->
+				
+				
+				<div class="col-sm-4 feature">
+					<div class="panel">
+						<div class="panel-heading">
+							<h3 class="panel-title"><span class="glyphicon glyphicon-user"></span> Collaborate online</h3>
+						</div> <!-- end panel-heading -->
+						
+						
+						<p>Share a sitemap with your client, and they can create, edit, and take away pages when planning their website. It&#8217;s then easy for you to jump in with some feedback, or lend a hand when needed.</p>
+						
+						
+					</div> <!-- end panel -->
+					
+				</div> <!-- end col-sm-4 -->
+				
+				
+				<div class="col-sm-4 feature">
+					<div class="panel">
+						<div class="panel-heading">
+							<h3 class="panel-title"><span class="glyphicon glyphicon-thumbs-up"></span> Easy to use</h3>
+						</div> <!-- end panel-heading -->
+						
+						
+						
+						<p>Let your clients loose on our super easy website planning tool, without wasting time training them on how to use it. Keep your clients happy, and your website planning process simple.</p>
+						
+						
+					</div> <!-- end panel -->
+					
+				</div> <!-- end col-sm-4 -->
+			</div> <!-- end container -->
+		</div> <!-- end features -->
+	
+	
+	
+		
+
+<div class="row" id="row-last">
+	<div class="container">
+		<div class="col-12">
+			<h2>What users say</h2>
+		
+
+			<div class="col-sm-6">
+				<p>"We've used Writemaps for several years and it's saved us a lot of time. 
+                    Because it's online and simple for our clients to use, the process of putting 
+                    a sitemap together is much quicker, it's become 
+                    one of the essential tools in our website development process."</p>
+                   <span><h5>Matt M, Director</h5></span>
+                    <a href="http://www.practicepl.us" >
+                <img class="thumbnail" src="images/PP_logo_2012.png" align="right"></a>
+				
+			</div>
+				
+			<div class="col-sm-6">
+					<p>"I came across WriteMaps and used it within my team. I couldn't keep a great tool like this secret, and now our whole company uses it."</p>
+					<h5>Paul W, Online Marketing</h5>
+					<a href="http://www.urbanelement.com" >
+        	      <img class="thumbnail" src="images/ue-logo-06.png" align="right" ></a>
+			</div>
+				
+		</div> <!-- end col-12 -->
+	</div> <!-- end container -->
+</div> <!-- end row-last -->
+
+
+      
+<!-- FOOTER -->
+	<footer>
+		<div class="container" >
+			<div class="row" id="footer-ul-content">
+				
+				<div class="col-sm-3">
+					<h6>LEARN MORE</h6>
+					<ul class="unstyled">
+						<li><a href="#">Features</a></li>
+						<li><a href="#">Pricing</a></li>
+					</ul>
+					
+				</div> <!-- end col-sm-3 -->
+				
+				<div class="col-sm-3">
+					<h6>ABOUT</h6>
+					<ul class="unstyled">
+						<li><a href="#">About WriteMaps</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+					
+				</div> <!-- end col-sm-3 -->
+				
+				<div class="col-sm-3">
+					<h6>LEGAL POLICIES</h6>
+					<ul class="unstyled">
+						<li><a href="#">Terms of Service</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+					</ul>
+					
+					
+				</div> <!-- end col-sm-3 -->
+				
+				<div class="col-sm-3">
+					<h6>SUPPORT</h6>
+					<ul class="unstyled">
+						<li><a href="#">FAQs</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+					
+					
+				</div> <!-- end col-sm-3 -->
+				
+				
+			</div> <!-- end row -->
+			
+			<div class="row">
+			<div class="col-sm-12" id="copyright">
+					<h6>&copy; 2014 Writemaps. All Rights Reserved.</h6>
+					
+				</div> <!-- end col-sm-12 -->
+			</div>
+			
+			
+		</div> <!-- end container -->
+	</footer>
+<!-- / FOOTER -->
+   
+
+<!-- All Javascript at the bottom of the page for faster page loading -->
+		
+	<!-- First try for the online version of jQuery-->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	
+	<!-- If no online access, fallback to our hardcoded version of jQuery -->
+	<script>window.jQuery || document.write('<script src="includes/js/jquery-1.8.2.min.js"><\/script>')</script>
+	
+	<!-- Bootstrap JS -->
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	
+	<!-- Custom JS -->
+	<script src="includes/js/script.js"></script>
+
+
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js" type="text/javascript"></script> 
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+
+<script src="assets/js/jquery.parallax-1.1.3.js" type="text/javascript" ></script>
+<script src="assets/js/jquery.localscroll-1.2.7-min.js" type="text/javascript" ></script>
+<script src="assets/js/jquery.scrollTo.min.js" type="text/javascript" ></script>
+<!-- PAGE CUSTOM SCROLLER -->
+<script type="text/javascript" src="assets/js/jquery.nicescroll.min.js"></script>
+
+
+<script>
+//Minimizes the menu on the mobile after clicking
+jQuery('.nav-collapse .nav > li > a').click(function(){
+		jQuery('.collapse.in').removeClass('in').css('height', '0');
+	});
+</script>
+
+<script>
+	jQuery('.carousel').carousel({
+  interval: 7000
+})
+</script>
+
+</body>
+</html>
