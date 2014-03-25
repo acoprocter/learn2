778d2e785fc866ed291f1304240119a8d87d6f1c
acoprocter <acoprocter@gmail.com>
2014-03-25 14:06:18 +1300

    asdf

--- header.php
+++ header.php
@@ -1,28 +1,4 @@
-<?php 
 
-ini_set("DISPLAY_ERRORS", "ON");
-
-$mysqli = new mysqli("localhost", "procter_learn2", "learn2", "procter_learn2");
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-/* Insert rows */
-$result=$mysqli->query("SELECT * FROM pages");
-
-
-$page=array();
-
-while ($row = $result->fetch_assoc()) {
-        $page[$row["name"]]=$row;
-	
-}
-
-
-?>
 
 <!DOCTYPE html>
 
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php 
+
+ini_set("DISPLAY_ERRORS", "ON");
+
+$active = $_GET["p"];
+
+$mysqli = new mysqli("localhost", "procter_learn2", "learn2", "procter_learn2");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* Insert rows */
+$result=$mysqli->query("SELECT * FROM pages");
+
+
+$page=array();
+
+while ($row = $result->fetch_assoc()) {
+        $page[$row["name"]]=$row;
+	
+}
+
+
+?>
+
 <?php require_once 'header.php'; ?>
 
 <div class="row" id="row1" >
